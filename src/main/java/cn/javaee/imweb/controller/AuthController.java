dcfa0e0c65eff4d3038276ba4cede145a6fc6fa8
qiuzj <zhanjia21@163.com>
2020-08-16 15:38:40 +0800

    动态配置IM服务端地址

--- src/main/java/cn/javaee/imweb/controller/AuthController.java
+++ src/main/java/cn/javaee/imweb/controller/AuthController.java
@@ -1,6 +1,9 @@
 package cn.javaee.imweb.controller;
 
 import cn.javaee.imweb.common.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -13,6 +16,11 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class AuthController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
+    @Value("${imserver.websocket.url}")
+    private String wsURL;
+
     @GetMapping("/")
     public String index(@RequestParam(name = "username", required = false)
                                       String username, HttpSession session) {
@@ -23,16 +31,34 @@ public class AuthController {
         }
     }
 
+    /**
+     * 登录
+     *
+     * @param username
+     * @param password
+     * @param model
+     * @param session
+     * @return
+     */
     @RequestMapping("/login")
     public String login(@RequestParam String username, String password, Model model, HttpSession session) {
         if (!StringUtils.hasText(username)) {
             return "login";
         }
+
         model.addAttribute("username", username);
+        model.addAttribute("wsURL", wsURL);
         session.setAttribute(Constants.SESSION_KEY, username);
+
         return "index";
     }
 
+    /**
+     * 退出
+     *
+     * @param session
+     * @return
+     */
     @RequestMapping("/logout")
     public String login(HttpSession session) {
         session.removeAttribute(Constants.SESSION_KEY);
